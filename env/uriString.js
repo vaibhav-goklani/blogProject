2ee9c9a88a34d75348c088e397da67ccbd146325
Vaibhav Goklani <128278054+vaibhav-goklani@users.noreply.github.com>
2024-02-03 19:18:17 +0530

    Add files via upload

---
+++ env/uriString.js
@@ -0,0 +1,3 @@
+const dbURI = "mongodb://vaibhav:1314188585@ac-hs2javw-shard-00-00.8b4no5e.mongodb.net:27017,ac-hs2javw-shard-00-01.8b4no5e.mongodb.net:27017,ac-hs2javw-shard-00-02.8b4no5e.mongodb.net:27017/?ssl=true&replicaSet=atlas-7n0zz8-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+module.exports = dbURI;
